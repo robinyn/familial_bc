3a5c916946fcb8ac566e857b76bd945a86f490f7
Robin <robinynhan@gmail.com>
2023-10-03 11:35:03 +0200

    Scripts modified

--- scripts/parse_annot.py
+++ scripts/parse_annot.py
@@ -43,7 +43,7 @@ def parse_vcf(input_dir, output_file):
         with open(output_file, "w") as output_tsv:
             output_tsv.write(header)
             for index, file in enumerate(files_list):
-                print("Processing {} ({}/{})".format(file, index+1, len(files_list)), end="\r", flush=True)
+                print("Processing {} ({}/{})".format(file, index+1, len(files_list)))
                 with open("{dir}/{name}".format(dir=input_dir, name=file), "r") as input_vcf:
                     for line in input_vcf:
                         # Disregard metadata/header lines
