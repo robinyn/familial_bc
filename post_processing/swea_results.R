b85eeed211496f43a20639dd8f4235cdfd7857f0
Robin <robinynhan@gmail.com>
2023-10-30 11:14:17 +0100

    Scripts modified

---
+++ post_processing/swea_results.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(ggplot2)
+
+setwd("~/Desktop/thesis_resources")
+
+per_variant=read_tsv("swea_results/per_variant_summary.tsv")
+per_transcript=read_tsv("swea_results/per_variant_summary.tsv")
